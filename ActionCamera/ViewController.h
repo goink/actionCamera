df5fb5c0a77abde5bd7f833925d03ed40965acb3
范桂盛 <fan.guisheng@xiaoyi.com>
2016-01-18 21:50:59 +0800

    Initial Commit

---
+++ ActionCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActionCamera
+//
+//  Created by 范桂盛 on 16/1/18.
+//  Copyright © 2016年 AC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ActionCamera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ActionCamera
+//
+//  Created by 范桂盛 on 16/1/18.
+//  Copyright © 2016年 AC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
